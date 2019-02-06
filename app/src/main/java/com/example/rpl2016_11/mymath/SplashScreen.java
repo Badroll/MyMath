5a737930196ef3d4dcc299950a0da1bab16e6ced
Badroll <badrl.xrpl2017smkrus@gmail.com>
2019-02-06 18:41:39 +0700

    MyMath

---
+++ app/src/main/java/com/example/rpl2016_11/mymath/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.rpl2016_11.mymath;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.ImageView;
+
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        tvSplash = (ImageView) findViewById(R.id.imageSplash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+    }
+}
---
+++ app/src/main/java/com/example/rpl2016_11/mymath/recViewBDatar/ItemClick.java
@@ -0,0 +1,7 @@
+package com.example.rpl2016_11.mymath.recViewBDatar;
+
+import android.view.View;
+
+public interface ItemClick  {
+    void onClick(View view, int position);
+}
